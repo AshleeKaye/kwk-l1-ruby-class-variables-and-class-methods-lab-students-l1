83ca286977859b0236ea45b556068516d5aa8996
AshleeKaye <ashleemk@hotmail.com>
2018-06-08 20:32:35 +0000

    automatically backed up by learn

---
+++ loggins.rb
@@ -0,0 +1,13 @@
+class User
+  
+  def name=(username)
+    @the_username = username
+  end
+  def name
+    @the_username
+  end
+end
+
+jessica = User.new
+jessica.name = jessica
+puts jessica.name
